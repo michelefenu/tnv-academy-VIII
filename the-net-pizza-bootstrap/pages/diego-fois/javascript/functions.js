053578f1e5f5a0cebc3aa190248ed4eaff6962bd
Michele Fenu <michelefenu@gmail.com>
2022-11-07 18:13:37 +0100

    Add bootsrap

---
+++ the-net-pizza-bootstrap/pages/diego-fois/javascript/functions.js
@@ -0,0 +1,31 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+ 
+
+
+function scrollFunction() {
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.height = "35px";
+    document.getElementById("links").style.fontSize = "20px";
+    document.getElementById("nameAndDescr").style.visibility = "hidden";
+    document.getElementById("choice").style.visibility = "hidden";
+    document.getElementById("b").style.backgroundImage = "url(../diego-fois/images/background-image.jpg)";
+
+  } else {
+    document.getElementById("navbar").style.height = "100%";
+    document.getElementById("links").style.fontSize = "30px";
+    document.getElementById("nameAndDescr").style.visibility = "visible";
+    document.getElementById("choice").style.visibility = "visible";
+    document.getElementById("b").style.backgroundImage = null;
+    document.getElementById("b").style.paddingTop = "44%";
+  } 
+}
+
+
+
+
+
+/* *******************   carosel   ************************* */
+
