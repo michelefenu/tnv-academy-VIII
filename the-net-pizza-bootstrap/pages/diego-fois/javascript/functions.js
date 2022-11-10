d02c64b5f48429f453b5a360ce7b2a4abf870e1c
Diego <diegoeffe96@gmail.com>
2022-11-10 17:39:08 +0100

    bootsrap-p-part

--- the-net-pizza-bootstrap/pages/diego-fois/javascript/functions.js
+++ the-net-pizza-bootstrap/pages/diego-fois/javascript/functions.js
@@ -6,26 +6,39 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.height = "35px";
-    document.getElementById("links").style.fontSize = "20px";
-    document.getElementById("nameAndDescr").style.visibility = "hidden";
-    document.getElementById("choice").style.visibility = "hidden";
-    document.getElementById("b").style.backgroundImage = "url(../diego-fois/images/background-image.jpg)";
+  if (document.body.scrollTop > 110 || document.documentElement.scrollTop > 110) {
+    document.getElementById("beforeScrollNav").style.visibility = "visible";
+    document.getElementById("afterScrollNav").style.visibility = "hidden";
+    document.getElementById("beforeScrollImg").style.visibility = "hidden";
+    document.getElementById("afterScrollFooS").style.visibility = "visible";
+    document.getElementById("afterScrollFooB").style.visibility = "visible";
 
   } else {
-    document.getElementById("navbar").style.height = "100%";
-    document.getElementById("links").style.fontSize = "30px";
-    document.getElementById("nameAndDescr").style.visibility = "visible";
-    document.getElementById("choice").style.visibility = "visible";
-    document.getElementById("b").style.backgroundImage = null;
-    document.getElementById("b").style.paddingTop = "44%";
+    document.getElementById("beforeScrollNav").style.visibility = "hidden";
+    document.getElementById("afterScrollNav").style.visibility = "visible";
+    document.getElementById("beforeScrollImg").style.visibility = "visible";
+    document.getElementById("afterScrollFooS").style.visibility = "hidden";
+    document.getElementById("afterScrollFooB").style.visibility = "hidden";
   } 
 }
 
 
+function  onClickFunc(element){
+    let countOfClicks = element.count;
 
 
+if(isEven(countOfClicks) === true){
+    element.style.visibility = "visible"
+} else {
+    element.style.visibility = "hidden"}
+}
 
-/* *******************   carosel   ************************* */
 
+function isEven(num){
+    if (num % 2 === 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
